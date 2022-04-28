4cbbe3bcd23c981d59a759a2c1839d131c84de6d
David Liang <dliang@diconcloud.com>
2022-04-28 15:43:38 -0700

    update build script and gitignore

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# download dependencies
+wget https://agentpp.com/download/snmp++-3.4.7.tar.gz 
+wget https://agentpp.com/download/agent++-4.5.1.tar.gz 
+
+# extract
+mkdir -p dependencies snmp++-3.4.7/build agent++-4.5.1/build build
+tar -xf snmp++-3.4.7.tar.gz 
+tar -xf agent++-4.5.1.tar.gz 
+
+# build dependencies
+cd snmp++-3.4.7/build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../dependencies
+make -j8
+make install 
+cd ../..
+
+cd agent++-4.5.1/build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../dependencies
+make -j8
+make install 
+cd ../..
+
+# build code
+cd build 
+cmake .. -DCMAKE_INSTALL_PREFIX=../dependencies
+make -j8
+cd ..
+
+echo "finished building"
+rm snmp++-3.4.7.tar.gz 
+rm agent++-4.5.1.tar.gz 
